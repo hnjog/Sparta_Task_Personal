d6c9564f93f4b34ef12132568f2bb05a3bb23304
Hyun <hnjogo@gmail.com>
2025-11-24 17:07:32 +0900

    [Init] Asset, gitIgnore, lfs setting

---
+++ TagNChase/Source/TagNChase/TagNChase.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TagNChase.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TagNChase, "TagNChase" );
