4f65def1f1d274a63a6865e710e27c877dd04474
Hyun <hnjogo@gmail.com>
2025-11-24 20:35:19 +0900

    [Add] Logging 용 매크로 추가

--- TagNChase/Source/TagNChase/TagNChase.cpp
+++ TagNChase/Source/TagNChase/TagNChase.cpp
@@ -4,3 +4,9 @@
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TagNChase, "TagNChase" );
+
+#pragma region NetLogging
+
+DEFINE_LOG_CATEGORY(LogTNNet);
+
+#pragma endregion
