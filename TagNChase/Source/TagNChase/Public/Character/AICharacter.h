04691d101b39ee84a1e65c23bedb04637001b33d
Hyun <hnjogo@gmail.com>
2025-12-01 18:42:35 +0900

    [Add] AICharacter

---
+++ TagNChase/Source/TagNChase/Public/Character/AICharacter.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/BaseCharacter.h"
+#include "AICharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TAGNCHASE_API AAICharacter : public ABaseCharacter
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
